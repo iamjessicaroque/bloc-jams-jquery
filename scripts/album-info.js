8ee19d783026ac29e5fc1dadef6fca888996ca24
Jessica Roque <jroque.dev@gmail.com>
2017-10-30 14:54:44 -0700

    Assignment 11 completed: added artist and release info.

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,6 +3,7 @@ $( document ).ready
     {
       $('#album-title').text(album.title);
       $('img#album-cover-art').attr('src', album.albumArtUrl);
-
+      $('.artist').text(album.artist);
+      $('#release-info').text(album.releaseInfo);
     }
 );
