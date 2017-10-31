720770b932f939b7118c13882ee1805b9f9e3586
Jessica Roque <jroque.dev@gmail.com>
2017-10-31 12:50:32 -0700

    Assignment 13: Added functionality for previous button on the player bar.

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -17,10 +17,29 @@ $( document ).ready
 
                      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
                      const nextSongIndex = currentSongIndex + 1;
-                     if (nextSongIndex >= album.songs.length) { return; }
-                     
+
+                     if (nextSongIndex >= album.songs.length)
+                        { return; }
+
                      const nextSong = album.songs[nextSongIndex];
                      player.playPause(nextSong);
+
+                  }
+               );
+            $('button#previous').click
+               ( function()
+                  {
+                     if (player.playState !== 'playing')
+                        { return; }
+            
+                     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+                     const prevSongIndex = currentSongIndex - 1;
+
+                     if (prevSongIndex < 0)
+                        { return; }
+
+                     const prevSong = album.songs[prevSongIndex];
+                     player.playPause(prevSong);
 
                   }
                );
